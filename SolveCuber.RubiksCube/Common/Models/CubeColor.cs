ecdc73421bcf8fc5377adc4ef08315768956ced0
Cube <wieszalajakub@gmail.com>
2025-03-09 18:28:16 +0100

    cubecolor, cubeface

---
+++ SolveCuber.RubiksCube/Common/Models/CubeColor.cs
@@ -0,0 +1,11 @@
+﻿namespace SolveCuber.RubiksCube.Common.Models;
+
+public enum CubeColor
+{
+    White = 1,
+    Yellow = 2,
+    Green = 3,
+    Blue = 4,
+    Red = 5,
+    Orange = 6
+}
---
+++ SolveCuber.RubiksCube/Common/Models/CubeFace.cs
@@ -0,0 +1,51 @@
+﻿namespace SolveCuber.RubiksCube.Common.Models;
+
+public class CubeFace
+{
+    public CubeColor[,] Face { get; private set; }
+
+    public CubeFace(int size, CubeColor color)
+    {
+        Face = new CubeColor[size, size];
+
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                Face[i, j] = color;
+            }
+        }
+    }
+
+    public CubeFace RotateClockwise()
+    {
+        int size = Face.GetLength(0);
+        CubeColor[,] currentFace = Face;
+
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                Face[i, j] = currentFace[2 - j, i];
+            }
+        }
+
+        return this;
+    }
+
+    public CubeFace RotateCounterClockwise()
+    {
+        int size = Face.GetLength(0);
+        CubeColor[,] currentFace = Face;
+
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                Face[i, j] = currentFace[j, 2 - i];
+            }
+        }
+
+        return this;
+    }
+}
