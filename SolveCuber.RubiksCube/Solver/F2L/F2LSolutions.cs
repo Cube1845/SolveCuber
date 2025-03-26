79d1c2789c985746528fac178fbb39db8db4d110
Cube <wieszalajakub@gmail.com>
2025-03-26 22:05:07 +0100

    more f2l solutions

--- SolveCuber.RubiksCube/Solver/F2L/F2LSolutions.cs
+++ SolveCuber.RubiksCube/Solver/F2L/F2LSolutions.cs
@@ -20,7 +20,28 @@ internal static class F2LSolutions
         public static List<CubeMove> LeftHand_1 = [CubeMove.U_, CubeMove.F, CubeMove.U_, CubeMove.F_, CubeMove.U, CubeMove.L_, CubeMove.U_, CubeMove.L];
         public static List<CubeMove> RightHand_1 = [CubeMove.U_, CubeMove.R, CubeMove.U, CubeMove.R_, CubeMove.U, CubeMove.R, CubeMove.U, CubeMove.R_];
 
-        //public static List<CubeMove> LeftHand_ThreeMover = [CubeMove.L_, CubeMove.U_, CubeMove.L];
-        //public static List<CubeMove> RightHand_ThreeMover = [CubeMove.R, CubeMove.U, CubeMove.R_];
+        public static List<CubeMove> LeftHand_2 = [CubeMove.L, CubeMove.U2, CubeMove.L2, CubeMove.U_, CubeMove.L2, CubeMove.U_, CubeMove.L];
+        public static List<CubeMove> RightHand_2 = [CubeMove.R_, CubeMove.U2, CubeMove.R2, CubeMove.U, CubeMove.R2, CubeMove.U, CubeMove.R];
+
+        public static List<CubeMove> LeftHand_3 = [CubeMove.U, CubeMove.L_, CubeMove.U, CubeMove.L, CubeMove.U_, CubeMove.L_, CubeMove.U_, CubeMove.L];
+        public static List<CubeMove> RightHand_3 = [CubeMove.U_, CubeMove.R, CubeMove.U_, CubeMove.R_, CubeMove.U, CubeMove.R, CubeMove.U, CubeMove.R_];
+    }
+
+    internal static class Case2
+    {
+        public static List<CubeMove> LeftHand_1 = [CubeMove.U, CubeMove.L_, CubeMove.U_, CubeMove.L, CubeMove.U2, CubeMove.L_, CubeMove.U, CubeMove.L];
+        public static List<CubeMove> RightHand_1 = [CubeMove.U_, CubeMove.R, CubeMove.U, CubeMove.R_, CubeMove.U2, CubeMove.R, CubeMove.U_, CubeMove.R_];
+
+        public static List<CubeMove> LeftHand_2 = [CubeMove.U, CubeMove.L_, CubeMove.U2, CubeMove.L, CubeMove.U2, CubeMove.L_, CubeMove.U, CubeMove.L];
+        public static List<CubeMove> RightHand_2 = [CubeMove.U_, CubeMove.R, CubeMove.U2, CubeMove.R_, CubeMove.U2, CubeMove.R, CubeMove.U_, CubeMove.R_];
+    }
+
+    internal static class Case3
+    {
+        public static List<CubeMove> LeftHand_1 = [CubeMove.U_, CubeMove.L_, CubeMove.U2, CubeMove.L, CubeMove.U_, CubeMove.L_, CubeMove.U, CubeMove.L];
+        public static List<CubeMove> RightHand_1 = [CubeMove.U, CubeMove.R, CubeMove.U2, CubeMove.R_, CubeMove.U, CubeMove.R, CubeMove.U_, CubeMove.R_];
+
+        public static List<CubeMove> LeftHand_2 = [CubeMove.U2, CubeMove.L_, CubeMove.U_, CubeMove.L, CubeMove.U_, CubeMove.L_, CubeMove.U, CubeMove.L];
+        public static List<CubeMove> RightHand_2 = [CubeMove.U2, CubeMove.R, CubeMove.U, CubeMove.R_, CubeMove.U, CubeMove.R, CubeMove.U_, CubeMove.R_];
     }
 }
