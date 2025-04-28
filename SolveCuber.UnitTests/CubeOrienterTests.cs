b758460c325f0d84faf110d32395d608c0e453fd
Cube <wieszalajakub@gmail.com>
2025-04-28 21:05:25 +0200

    tests for scrambler & orienter

---
+++ SolveCuber.UnitTests/CubeOrienterTests.cs
@@ -0,0 +1,49 @@
+﻿using SolveCuber.Common;
+using SolveCuber.CubeModel.Models;
+using SolveCuber.CubeModel;
+using SolveCuber.Solver.Common;
+
+namespace SolveCuber.UnitTests;
+
+public class CubeOrienterTests
+{
+    [Fact]
+    public void OrientCube_WithUpColor_ShouldOrientCubeCorrectly()
+    {
+        Cube cube = new();
+
+        var moves = CubeOrienter.OrientCube(cube, CubeColor.Yellow);
+
+        moves.ShouldNotBeNull();
+        cube.Up.Face[1, 1].ShouldBe(CubeColor.Yellow);
+    }
+
+    [Fact]
+    public void OrientCube_WithFrontAndUpColor_ShouldOrientCubeCorrectly()
+    {
+        Cube cube = new();
+
+        var moves = CubeOrienter.OrientCube(cube, CubeColor.Blue, CubeColor.Yellow);
+
+        moves.ShouldNotBeNull();
+        cube.Up.Face[1, 1].ShouldBe(CubeColor.Yellow);
+        cube.Front.Face[1, 1].ShouldBe(CubeColor.Blue);
+    }
+
+    [Theory]
+    [InlineData(CubeColor.Red, CubeColor.Orange)]
+    [InlineData(CubeColor.Blue, CubeColor.Green)]
+    [InlineData(CubeColor.White, CubeColor.Yellow)]
+    public void OrientCube_WithIncorrectColors_ShouldThrowException(CubeColor frontColor, CubeColor upColor)
+    {
+
+        Cube cube = new();
+
+        var action = () =>
+        {
+            CubeOrienter.OrientCube(cube, frontColor, upColor);
+        };
+
+        action.ShouldThrow<RubiksCubeException>();
+    }
+}
---
+++ SolveCuber.UnitTests/ScramblerTests.cs
@@ -0,0 +1,40 @@
+﻿using SolveCuber.CubeModel.Models;
+using SolveCuber.Scramble;
+
+namespace SolveCuber.UnitTests;
+
+public class ScramblerTests
+{
+    [Fact]
+    public void ScrambleCube_ShouldScrambleCubeWith25Moves()
+    {
+        Cube cube = new();
+
+        var scramble = Scrambler.ScrambleCube(cube);
+
+        scramble.Count.ShouldBe(25);
+    }
+
+    [Fact]
+    public void GenerateScramble_ShouldGenerateCorrectLength()
+    {
+        const int scrambleLength = 10;
+
+        var scramble = Scrambler.GenerateScramble(scrambleLength);
+
+        scramble.Count.ShouldBe(scrambleLength);
+    }
+
+    [Fact]
+    public void GenerateScramble_ShouldNotHaveSameFaceConsecutive()
+    {
+        var scramble = Scrambler.GenerateScramble(50);
+
+        for (int i = 1; i < scramble.Count; i++)
+        {
+            var prevFace = scramble[i - 1].ToString().TrimEnd('_', '2');
+            var currFace = scramble[i].ToString().TrimEnd('_', '2');
+            currFace.ShouldNotBe(prevFace);
+        }
+    }
+}
